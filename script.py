dc1bc1473308c690e8b74d0ae99f0a92cbdef270
LM Train <layconmm@msn.com>
2020-01-26 20:41:30 -0600

    Various examples

--- script.py
+++ script.py
@@ -6,3 +6,21 @@ for number in numbers:
 
 for i in range(3):
   print(i)
+
+numbers = [2, 4, 6, 8]
+for number in numbers:
+  print("hello!")
+
+
+numbers = [1, 1, 2, 3]
+for number in numbers:
+  if number % 2 == 0:
+    continue
+  print(number)
+
+drink_choices = ["coffee", "tea", "water", "juice", "soda"]
+for drink in drink_choices:
+  print(drink)
+
+for i in range(3):
+  print(5)
