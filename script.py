2b038e50ac0c52f43a2000acc9d8cbd7079a8954
LM Train <layconmm@msn.com>
2020-01-26 20:28:39 -0600

    Loops 2

--- script.py
+++ script.py
@@ -1,9 +1,5 @@
-single_digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-squares = []
-for numbers in single_digits:
-  squares.append(numbers**2)
-print(squares)
-
-cubes = [digits **3 for digits in single_digits]
-
-print(cubes)
+numbers = [1, 1, 2, 3]
+for number in numbers:
+  if number % 2 == 0:
+    break
+  print(number)
