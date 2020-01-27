c4c25003e79c9fcba4becffb92d12dfbc975e87d
LM Train <layconmm@msn.com>
2020-01-26 20:50:57 -0600

    Code Challenge: Loops

--- script.py
+++ script.py
@@ -1,28 +1,9 @@
-hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
-
-prices = [30, 25, 40, 20, 20, 35, 50, 35]
-
-last_week = [2, 3, 5, 8, 4, 4, 6, 2]
-
-# Prices and Cuts:
-total_price = 0
-for price in prices:
-  total_price += price
-  #print(total_price)
-average_price = total_price / len(prices)
-#print("Average Haircut Price: " + str(average_price))
-print("Average Haircut Price: ${0}".format(average_price))
-#number_of_prices = len(prices)
-new_prices = [cut_price - 5 for cut_price in prices]
-print(new_prices)
-
-#Revenue:
-total_revenue = 0
-for i in range(len(hairstyles)):
-  total_revenue += prices[i] * last_week[i]
-  #print(total_revenue)
-print("Total Revenue: ${0}".format(total_revenue))
-average_daily_revenue = total_revenue / 7
-print("Average Daily Revenue: ${0}".format(average_daily_revenue))
-cuts_under_30 = [hairstyles[i] for i in range(len(hairstyles)) if new_prices[i] < 30]
-print(cuts_under_30)
+#Write your function here
+def reversed_list(lst1, lst2):
+  for index in range(len(lst1)):
+    if lst1[index] != lst2[len(lst2) - 1 - index]:
+      return False
+  return True
+#Uncomment the lines below when your function is done
+print(reversed_list([1, 2, 3], [3, 2, 1]))
+print(reversed_list([1, 5, 3], [3, 2, 1]))
