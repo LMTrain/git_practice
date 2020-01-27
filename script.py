49f8ece31e5e2e07efb966c11a9bd313c8d539e3
LM Train <layconmm@msn.com>
2020-01-26 19:31:56 -0600

    Code on Sals Shipping project

--- script.py
+++ script.py
@@ -1,11 +1,50 @@
-# Write your same_name function here:
-def same_name(your_name, my_name):
-  if (your_name == my_name):
-    return True
+# SAL'S SHIPPING
+# Ground Shipping Calculation
+def ground_shipping(weight):
+  if (weight <= 2):
+    return (weight * 1.50) + 20
+  elif (weight > 2) and (weight <= 6):
+    return (weight * 3) + 20
+  elif (weight > 6) and (weight <= 10):
+    return (weight * 4) + 20
+  elif (weight > 10):
+    return (weight * 4.75) + 20
+cost = ground_shipping(4.8)
+print(cost)
+
+prem_grd_shipping = 125
+
+# Drone Shipping Calculation
+def drone_shipping(weight):
+  if (weight <= 2):
+    return (weight * 4.50) + 0
+  elif (weight > 2) and (weight <= 6):
+    return (weight * 9) + 0
+  elif (weight > 6) and (weight <= 10):
+    return (weight * 12) + 0
+  elif (weight > 10):
+    return (weight * 14.75) + 0
+cost = drone_shipping(41.5)
+print(cost)
+ 
+def print_cheapest_shipping(weight):
+  ground = ground_shipping(weight)
+  premium = prem_grd_shipping
+  drone = drone_shipping(weight)
+  
+  if ground < premium and ground < drone:
+    method = "standard ground"
+    cost = ground
+  elif premium < ground and premium < drone:
+    method = "premium ground"
+    cost = premium
   else:
-    return False
-# Uncomment these function calls to test your same_name function:
-print(same_name("Colby", "Colby"))
-# should print True
-print(same_name("Tina", "Amber"))
-# should print False
+    method = "drone"
+    cost = drone
+  print(
+    "The cheapest option available is $%.2f with %s shipping,"
+    % (cost, method)
+  )
+print_cheapest_shipping(4.8)
+print_cheapest_shipping(41.5)
+    
