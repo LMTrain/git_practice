efe230671547b7371fb364ce9123f0195408bcfa
LM Train <layconmm@msn.com>
2020-01-26 20:26:32 -0600

    Loops

--- script.py
+++ script.py
@@ -1,6 +1,9 @@
-#Write your function here
-def every_three_nums(start):
-  return list(range(start, 101, 3))
+single_digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+squares = []
+for numbers in single_digits:
+  squares.append(numbers**2)
+print(squares)
 
-#Uncomment the line below when your function is done
-print(every_three_nums(91))
+cubes = [digits **3 for digits in single_digits]
+
+print(cubes)
